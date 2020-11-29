46551b560dc6d943826cb87e7e2260197688f24f
AlexanderCm1 <victorcampos@upeu.edu.pe>
2020-11-28 23:03:01 -0500

    safo :)

--- src/app/components/menu-evaluacion/menu-evaluacion.component.ts
+++ src/app/components/menu-evaluacion/menu-evaluacion.component.ts
@@ -3,6 +3,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { MatSidenav } from '@angular/material/sidenav';
+import { element } from 'protractor';
 
 
 @Component({
@@ -12,12 +13,18 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class MenuEvaluacionComponent implements OnInit {
 
-  opened = true;
+  opened = false;
   @ViewChild('sidenav') sidenav:MatSidenav;
-  isExpanded = true;
+  isExpanded = false;
   showSubmenu: boolean = false;
   isShowing = false;
   showSubSubMenu: boolean = false;
+
+  @ViewChild('sidenav') sidenav1:MatSidenav;
+  isExpanded1 = false;
+  showSubmenu1: boolean = false;
+  isShowing1 = false;
+
 
   constructor(){
   
@@ -30,13 +37,26 @@ export class MenuEvaluacionComponent implements OnInit {
     if (!this.isExpanded) {
       this.isShowing = true;
     }
+    if (!this.isExpanded1) {
+      this.isShowing1 = true;
+    }
   }
 
   mouseleave() {
     if (!this.isExpanded) {
       this.isShowing = false;
     }
+    if (!this.isExpanded1) {
+      this.isShowing1 = false;
+    }
   }
+
+  onSelect(){
+   this.showSubmenu = !this.showSubSubMenu;
+
+   //showSubmenu = !showSubmenu;
+  }
+
 }
 
 
